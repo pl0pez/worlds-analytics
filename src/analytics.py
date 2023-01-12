a182d06f56c5b5ee5e4085715890a61282258973
pl0pez <pedro.lpzgrc@gmail.com>
2023-01-12 10:47:42 +0000

    Team picks, refactoring, and args

--- src/analytics.py
+++ src/analytics.py
@@ -1,21 +1,45 @@
-import pandas as pd
 from collections import Counter
 import itertools
+import sys
+import pandas as pd
+
+
+# Bans columns
+bans_columns= ['BB1',  'BB2', 'BB3', 'BB4', 'BB5', 'RB1', 'RB2', 'RB3', 'RB4', 'RB5']
+blue_bans = ['BB1',  'BB2', 'BB3', 'BB4', 'BB5']
+red_bans = ['RB1', 'RB2', 'RB3', 'RB4', 'RB5']
+
+# Pick columns
+pick_columns = ['BP1', 'BP2', 'BP3', 'BP4', 'BP5', 'RP1', 'RP2', 'RP3', 'RP4', 'RP5']
+blue_picks = ['BP1', 'BP2', 'BP3', 'BP4', 'BP5']
+red_picks = ['RP1', 'RP2', 'RP3', 'RP4', 'RP5']
 
 def load_data(path='./data/worlds-1218.csv'):
     return pd.read_csv(path, sep=',', header=0)
 
-def count_bans(data, bans_columns, top=3):
+def count_times(data, columns, top=3):
     aux_list = []
-    for column in bans_columns: 
+    for column in columns: 
         aux_list.append(data[column].values.tolist())
     
     aux_list = list(itertools.chain.from_iterable(aux_list)) #joining the list of lists into unique list!
 
     return list(sorted(Counter(aux_list).items(),reverse=True, key=lambda x:x[1]))[0:top]
 
+def team_picks(data, name, side, top=5):
+    if side == 'Blue':
+        return count_times(data[data['Blue']==name], blue_picks, top)
+    if side == 'Red':
+        return count_times(data[data['Red']==name], red_picks, top)
+    if side == '':
+        return count_times(data[(data['Blue']==name) | (data['Red']==name)], pick_columns, top)
+
 def main():
-    data = load_data()
+    if sys.argv: 
+        args = sys.argv[0]
+    else: args = []
+    
+    data = load_data(args[0])
     print(data.head())
     
     # Columns info
@@ -28,25 +52,30 @@ def main():
     print("Percentage blue victories: {}".format(bv/len(data)))
     print("Percentage red victories: {}".format(rv/len(data)))
     
-    
-    bans_columns= ['BB1',  'BB2', 'BB3', 'BB4', 'BB5', 'RB1', 'RB2', 'RB3', 'RB4', 'RB5']
-    blue_bans = ['BB1',  'BB2', 'BB3', 'BB4', 'BB5']
-    red_bans = ['RB1', 'RB2', 'RB3', 'RB4', 'RB5']
+
     
     # Three most banned champions in general
-    print(count_bans(data, bans_columns))
+    print(count_times(data, bans_columns))
     
     # Three most banned champions by blue side
-    print(count_bans(data, blue_bans))
+    print(count_times(data, blue_bans))
     
     # Three most banned champions by red side
-    print(count_bans(data, red_bans))
+    print(count_times(data, red_bans))
     
     # Three most picked champions in general
+    print(count_times(data, pick_columns,5))
     
     # Three most picked champions by blue side
+    print(count_times(data, blue_picks,5))
     
     # Three most picked champions by red side
-
+    print(count_times(data, red_picks,5))
+    
+    # Picks by team
+    print(team_picks(data, 'T1', 'Blue', -1))
+    print(team_picks(data, 'T1', 'Red', -1))
+    print(team_picks(data, 'T1', '', -1))
+          
 if __name__ == "__main__":
     main()
